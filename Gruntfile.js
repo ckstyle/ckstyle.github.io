0a3050615a5ae62149043aa8fc16b3198196044d
思竹 <zhifu.wzf@alibaba-inc.com>
2014-09-16 16:00:00 +0800

    clean

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+/*
+   package.json config
+   npm install
+   grunt
+ */
+
+module.exports = function(grunt) {
+
+  var fs = require('fs');
+  // Project configuration.
+
+  require('load-grunt-tasks')(grunt, {
+    pattern: 'grunt-contrib-*'
+  });
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      all: {
+        src: [
+          "js/lib/json2.js",
+          "js/lib/modernizr-2.0.6.js",
+          "js/lib/jquery.js",
+          "js/lib/sea.js",
+          "tools/bootstrap/js/bootstrap.js",
+          "tools/codemirror/lib/codemirror.js",
+          "tools/codemirror/mode/css/css.js",
+          "js/lib/highcharts.js",
+          "js/lib/mustache.js",
+          "js/dist/ckstyle.js",
+          "js/lib/cssmin.js",
+          "js/main.js"
+        ],
+        dest: 'js/all.js'  
+      }
+    },
+    clean: {
+      all: ["js/all.js", "js/all.min.js"]
+    },
+    uglify: {
+      all: {
+        files: {
+          'js/all.min.js': ['<%= concat.all.dest %>']  
+        }
+      }
+    },
+  });
+
+  grunt.registerTask('default', ['clean:all', 'concat:all', 'uglify:all']);
+};
--- handler/invite-code.php
+++
@@ -1,8 +0,0 @@
-<?php include 'helper.php'; ?>
-<?php
-	if (isset($_GET['user'])) {
-		echo md5($_GET['user'].time());
-	} else {
-		echo 'need user param';
-	}
-?>
